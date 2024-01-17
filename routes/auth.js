3a49db5345fa754071e8b3ac3c216fd5c821fe6b
baderaka <baadhirabaadhi98@gmail.com>
2024-01-17 16:34:45 +0400

    lastttt

--- routes/auth.js
+++ routes/auth.js
@@ -3,27 +3,24 @@ const jwt = require('jsonwebtoken')
 
 function verifytoken(req, res, next) {
     let authheader = req.headers['Authorization'];
-    if (authheader !== undefined) {
-        const bearertoken = authheader.split(''); // this splits the token and bearer.that we set in the header of the browser
-        const token = bearertoken[1]; // the 0th element is bearer then 1 st is our token
-        if (!token)
-            return res.json({
-                message: "no token available,first u need to login"
-            })
-        jwt.verify(token, "verysecret", (err, data) => {
-            if (err) {
-                return res.send(err);
-
-            } else {
-                req.id = data.id;
-
-
-            }
-            next();
-
+    const bearertoken = authheader.split(' '); // this splits the token and bearer.that we set in the header of the browser
+    const token = bearertoken[1]; // the 0th element is bearer then 1 st is our token
+    if (!token)
+        return res.json({
+            message: "no token available,first u need to login"
         })
-    }
+    jwt.verify(token, "verysecret", (err, data) => {
+        if (err) {
+            return res.send(err);
 
+        } else {
+            req.id = data.id;
+
+
+        }
+        next();
+
+    })
 
 }
 
